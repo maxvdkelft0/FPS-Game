a58e973d8c91a5a85a43a21cdf035c7c2ecb136a
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2023-11-20 12:43:39 +0100

    update playerMovement.cs
    - Whole rewrite script
    - Added Walk, Run, Jump & Idle functionallity

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,44 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine; 
+using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float currentSpeed;
-    public float walkingSpeed;
-    public float runningSpeed;
-    public float jumpSpeed;
-    public float gravity;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 7f;
 
-    public CharacterController characterController;
+    private Vector3 moveDirection;
+    private Vector3 moveDirectionZ;
+    private Vector3 moveDirectionX;
+    private Vector3 velocity;
 
-    private float baselineGravity;
-    private float xMove;
-    private float zMove;
-    private Vector3 move;
+    private float gravityValue = -9.81f;
+    private float jumpHeight = 2f;
+
+    private CharacterController characterController;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        currentSpeed = walkingSpeed;
-        baselineGravity = gravity;
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xMove = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
-        zMove = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
+        Move();
+    }
 
-        move = transform.right * xMove +
-            transform.up * gravity +
-            transform.forward * zMove;
-
-        characterController.Move(move);
-        if (Input.GetKey(KeyCode.Space) && characterController.isGrounded)
+    private void Move()
+    {
+        if(characterController.isGrounded && velocity.y < 0)
         {
-            // jump
+            velocity.y = -2f;
         }
+
+        float moveZ = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+
+        moveDirectionZ = new Vector3(0, 0, moveZ);
+        moveDirectionX = new Vector3(moveX, 0, 0);
+        moveDirection = transform.TransformDirection(moveDirectionZ + moveDirectionX);
+
+        
+        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Walk();
+        }
+        else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        if(characterController.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+            if (moveDirection != Vector3.zero)
+            {
+                Idle();
+            }
+        }
+
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravityValue * Time.deltaTime; // adds gravity
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveDirection *= walkSpeed;
+    }
+
+    private void Run()
+    {
+        moveDirection *= runSpeed;
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravityValue);
+    }
+
+    private void Idle()
+    {
 
     }
 }
