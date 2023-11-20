e8f30ceafbb361a259a29c41a8e9ec3756095656
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2023-11-20 09:54:47 +0100

    update
    - Added camera rotation.

---
+++ Assets/Scripts/cameraRotation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour
+{
+
+    public Transform player;
+    public float speed = 100f;
+
+    public Camera cam;
+
+    private float xMouse;
+    private float yMouse;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // hide cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMouse = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        yMouse = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+        xRotation -= yMouse;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * xMouse);
+    }
+}
