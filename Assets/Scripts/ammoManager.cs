576a7903aa7092669dc8df7c053b7886f6263705
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2023-11-30 10:23:39 +0100

    #6 feat(ammo-manager): Created the beginning and functional of the ammo manager

---
+++ Assets/Scripts/ammoManager.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // Add this line to include the UnityEngine.UI namespace
+
+public class ammoManager : MonoBehaviour
+{
+    public int maxAmmo = 10;
+    private int currentAmmo;
+
+    public TextMeshProUGUI ammoText; // Reference to the Text component
+
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+        UpdateAmmoUI();
+    }
+
+    public int GetCurrentAmmo()
+    {
+        return currentAmmo;
+    }
+
+    public void DecreaseAmmo()
+    {
+        if (currentAmmo > 0)
+        {
+            currentAmmo--;
+            UpdateAmmoUI();
+        }
+    }
+
+    public void ReloadAmmo()
+    {
+        currentAmmo = maxAmmo;
+        UpdateAmmoUI();
+    }
+
+    void UpdateAmmoUI()
+    {
+        // Update the UI Text with the current ammo count
+        if (ammoText != null)
+        {
+            ammoText.text = "Ammo: " + currentAmmo.ToString();
+        }
+    }
+}
--- Assets/Scripts/shooting.cs
+++ Assets/Scripts/shooting.cs
@@ -1,34 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class shooting : MonoBehaviour
 {
     public Camera cam;
+    public ammoManager ammoManager;
 
     private RaycastHit hit;
     private Ray ray;
 
-
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ray = cam.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit))
+            // Check if there is enough ammo to shoot
+            if (ammoManager.GetCurrentAmmo() > 0)
             {
-                if (hit.collider.tag.Equals("NPC"))
+                ray = cam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
                 {
-                    Destroy(hit.collider.gameObject);
-                    Debug.Log("Hits shit");
+                    if (hit.collider.tag.Equals("NPC"))
+                    {
+                        Destroy(hit.collider.gameObject);
+                        ammoManager.DecreaseAmmo(); // Decrease ammo when shooting
+                        Debug.Log("Hits shit");
+                    }
                 }
-                
+            }
+            else
+            {
+                StartCoroutine(ReloadAndDelay());
             }
         }
+
+        // Check if the player wants to reload
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ReloadAndDelay());
+        }
+    }
+
+    IEnumerator ReloadAndDelay()
+    {
+        Debug.Log("Reloading");
+        yield return new WaitForSeconds(2f); // Add a delay to simulate reloading time
+        ammoManager.ReloadAmmo(); // Reload ammo
+        Debug.Log("Reloaded");
     }
 }
